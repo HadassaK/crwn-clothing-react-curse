7e3cad15516e0304f8b869b4441aa2d88e90f607
Hadassa Itzkowitz <hi250066@ncr.com>
2023-05-01 08:38:43 +0300

    created authentication page component alongside sign in form.
     section 6 (103,104)

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,93 @@
+import './sign-in-form.styles.scss'
+import { useState } from "react";
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth, signInWithGooglePopup, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils'
+import FromInput from "../form-input/form-input.components";
+import Button from "../button/botton.component.jsx"
+
+const defaultFormFields = {
+    email:'',
+    password:'',
+}
+
+const SignInForm=()=>{
+    const[formFields, setFormFields] = useState(defaultFormFields);
+    const{email, password} = formFields;
+
+    console.log(formFields);
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    }
+
+    const signInWithGoogle = async() => {
+        try{
+            const {user} = await signInWithGooglePopup();
+            await createUserDocumentFromAuth(user)
+        }
+        catch (error) { console. error(error);
+     }
+    }
+
+    const handleSubmit = async (event)=>{
+        event.preventDefault();
+        
+        try{
+            const {user} = await signInAuthUserWithEmailAndPassword(email, password);
+            console.log(user);
+            resetFormFields();
+        }
+        catch(error){  
+            switch(error.code){
+                case 'auth/wrong-password':
+                    alert('incorrect password for email');
+                    break;
+                case 'auth/wrong-password':
+                    alert("incorrect password for email");
+                    break;
+                case 'auth/user-not-found':
+                    alert('no user associated with email');
+                    break;
+                default:
+                    console.log(error);
+            }
+        }
+        
+
+    }
+    
+    const handleChange = (event)=> {
+        const {name, value} = (event).target;
+        
+        setFormFields({...formFields, [name]: value });
+    }
+    return (
+        <div className='sign-in-container'>
+            <h2>Already have an account?</h2>
+            <span>Sign in with email and password</span>
+            <form onSubmit={handleSubmit}>
+                <FromInput
+                    label="Email"
+                    type="email"
+                    required
+                    onChange={handleChange}
+                    name="email"
+                    value={email}/>
+                
+                <FromInput 
+                    label="Password"
+                    type="password"
+                    required
+                    onChange={handleChange}
+                    name="password"
+                    value={password}/>
+
+            <div className='buttons-container'>
+                <Button type="submit">Sign In</Button>
+                <Button type="button" onClick={signInWithGoogle} buttonType="google">Google sign in</Button>
+            </div>
+            </form>
+        </div>
+    );
+}
+
+export default SignInForm;
