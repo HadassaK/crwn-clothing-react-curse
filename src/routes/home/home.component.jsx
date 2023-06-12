01e1a0bcf6fed3030e677999af239f9a74b854d6
Hadassa Itzkowitz <hi250066@ncr.com>
2023-06-12 10:57:01 +0300

    add and get data from firebase. display categories and create routing dynamically.

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Categories from '../../components/categories/categories.component'
+import Directory from '../../components/directory/directory.component'
 
 const Home =()=>{
     const categories=
@@ -34,7 +34,7 @@ const Home =()=>{
     return (
         <div>
             <Outlet/>
-            <Categories categories={categories}/>
+            <Directory categories={categories}/>
         </div>
         );
 };
